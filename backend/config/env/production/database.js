f626198403b886f0ec0208a61386d09326173eeb
anthoniomadu <31852341+anthoniomadu@users.noreply.github.com>
2020-09-22 17:29:18 +0100

    json

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "localhost"),
+        host: env("DATABASE_HOST", "ec2-3-216-92-193.compute-1.amazonaws.com"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "strapi"),
-        username: env("DATABASE_USERNAME", "strapi"),
-        password: env("DATABASE_PASSWORD", "strapi"),
+        database: env("DATABASE_NAME", "df64q6252f10t2"),
+        username: env("DATABASE_USERNAME", "tmkzhtfnbnkrsf"),
+        password: env("DATABASE_PASSWORD", "51c5ccc90d2f765eac74bbe621c893dea029b7f52f5021303019dc9b052e8c96"),
         schema: "public",
         ssl: { rejectUnauthorized: false }
       },
