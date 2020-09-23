de0b58a3fb9a8ee9ffb233cfb8461df7441b78fe
anthoniomadu <31852341+anthoniomadu@users.noreply.github.com>
2020-09-23 13:06:56 +0100

    database.js

--- backend/config/env/production/database.js
+++ backend/config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
       connector: "bookshelf",
       settings: {
         client: "postgres",
-        host: env("DATABASE_HOST", "ec2-54-172-173-58.compute-1.amazonaws.com"),
+        host: env("DATABASE_HOST", "ec2-3-216-92-193.compute-1.amazonaws.com"),
         port: env.int("DATABASE_PORT", 5432),
-        database: env("DATABASE_NAME", "d489errk8mqgmc"),
+        database: env("DATABASE_NAME", "df64q6252f10t2"),
         username: env("DATABASE_USERNAME", "tmkzhtfnbnkrsf"),
-        password: env("DATABASE_PASSWORD", "2f77a5c3232744c21798fc2c1765a51e52b3a10f73e955f6f1f287f0edbe31c7"),
+        password: env("DATABASE_PASSWORD", "51c5ccc90d2f765eac74bbe621c893dea029b7f52f5021303019dc9b052e8c96"),
         schema: "public",
         ssl: { rejectUnauthorized: false }
       },
