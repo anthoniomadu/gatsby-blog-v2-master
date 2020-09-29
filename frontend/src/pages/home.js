a45500de47570e8a981c4eba30775337c42a9130
anthoniomadu <31852341+anthoniomadu@users.noreply.github.com>
2020-09-29 22:40:53 +0100

    removed animation in home

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -32,12 +32,6 @@ class Home extends Component {
           <HomeCarousel />
         </Container>
         <Container>
-          <div className="yogapose2">
-            <img
-              src="https://res.cloudinary.com/deya21gvu/image/upload/v1601049128/LU%20pics/APNG/yogapose2_pxtzso.png"
-              alt="Peace"
-            ></img>
-          </div>
         </Container>
         <br />
         <div>
