1dc44392468bf6d752dd6172ff8d4f0cf94d5a31
kamsiyochim <69901028+kamsiyochim@users.noreply.github.com>
2020-09-30 16:54:00 +0100

    Update home.js

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,132 +1,98 @@
-import React, { Component } from "react"
-import HomeCarousel from "../containers/pages/HomeCarousel"
-import Aboutus from "./aboutus"
+import React from 'react';
+import HomeCarousel from '../containers/pages/HomeCarousel';
+import Aboutus from './aboutus';
+import emailjs from "emailjs-com";
 // import Reelcarousel from './Reelcarousel'
-import "./App.css"
-import { Container } from "react-bootstrap"
+import './App.css'
+import { Container } from 'react-bootstrap';
 
-class Home extends Component {
-  state = {
-    fname: "",
-    lname: "",
-    email: "",
-    message: "",
-  }
 
-  handleChange = (e) => {
-    this.setState({
-      [e.target.id]: e.target.value,
-    });
-  };
-
-  handleSubmit = (e) => {
+function sendEmail(e){
     e.preventDefault();
-    console.log(this.state);
-  }
 
-  render() {
-    return (
-      <Container class="row">
-        <br />
-        <Container>
-          <HomeCarousel />
-        </Container>
-        <Container>
-        </Container>
-        <br />
-        <div>
-          <Aboutus />
-        </div>
-        <br />
-        <Container class="Contactfooter">
-          <div class="container">
-            <div class="row">
-              <div class="col-md-5">
-                <div class="row">
-                  <div class="col-6"></div>
-                </div>
-                <ul class="nav">
-                  <li class="nav-item">
-                    <a href="" class="nav-link pl-0">
-                      <i class="fa fa-facebook fa-lg"></i>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="" class="nav-link">
-                      <i class="fa fa-twitter fa-lg"></i>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="" class="nav-link">
-                      <i class="fa fa-github fa-lg"></i>
-                    </a>
-                  </li>
-                  <li class="nav-item">
-                    <a href="" class="nav-link">
-                      <i class="fa fa-instagram fa-lg"></i>
-                    </a>
-                  </li>
-                </ul>
-                <br />
-              </div>
-              <div className="Contactus">
-                <p>Contact Me</p>
-                <div>
-                  <form onSubmit={this.handleSubmit}>
-                    <label htmlFor="fname">First Name</label>
-                    <input
-                      type="text"
-                      id="fname"
-                      name="firstname"
-                      placeholder="Your name.."
-                      onChange={this.handleChange}
-                    />
-                    <label htmlFor="sname">Last Name</label>
-                    <input
-                      type="text"
-                      id="lname"
-                      name="lastname"
-                      placeholder="Your last name.."
-                      onChange={this.handleChange}
-                    />
+    emailjs.sendForm('gmail', 'template_s4plr4a', e.target, 'user_NDMZL6KsYA29lZZeakAMV')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
+      e.target.reset()
+}
 
-                    <label htmlFor="email">Email</label>
-                    <input
-                      type="email"
-                      id="email"
-                      name="email"
-                      placeholder="Your email"
-                      onChange={this.handleChange}
-                    />
-
-                    <label htmlFor="textarea">Subject</label>
-                    <textarea
-                      id="subject"
-                      name="subject"
-                      placeholder="Write something.."
-                      onChange={this.handleChange}
-                    ></textarea>
-                    <input type="submit" value="submit" />
-                  </form>
-                </div>
-              </div>
+const Home = () => {
+    return(
+        
+        <Container class='row'>
+        <br/>
+            <Container>
+                <HomeCarousel/>
+            </Container>
+            <Container>
+            <div className="yogapose2">
+            <img src="https://res.cloudinary.com/deya21gvu/image/upload/v1601049128/LU%20pics/APNG/yogapose2_pxtzso.png" alt="Peace"></img>
             </div>
+            </Container>
+            <br/>
+             <div>
+                <Aboutus/>       
+            </div>
+            <br/>
+            <Container class="footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-5">
+                        <div class="row">
+                            <div class="col-6">
+                                
+                            </div>
+                            
+                        </div>
+                        <ul class="nav">
+                            <li class="nav-item"><a href="" class="nav-link pl-0"><i class="fa fa-facebook fa-lg"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-twitter fa-lg"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-github fa-lg"></i></a></li>
+                            <li class="nav-item"><a href="" class="nav-link"><i class="fa fa-instagram fa-lg"></i></a></li>
+                        </ul>
+                        <br/>
+                    </div>
+                    <div className="container">
+                    <form className="contact-form" onSubmit={sendEmail}>
+      <div className="row pt-5 mx-auto">
+          <div className="col-8 form-group mx-auto">
+              <input type="text" className="form-control" placeholder="Name" name="name"/>
           </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+              <input type="email" className="form-control" placeholder="Email Address" name="email"/>
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+              <input type="text" className="form-control" placeholder="Subject" name="subject"/>
+          </div>
+          <div className="col-8 form-group pt-2 mx-auto">
+              <textarea className="form-control" id="" cols="30" rows="8"  placeholder="Your message" name="message"></textarea>
+          </div>
+          <div className="col-8 pt-3 mx-auto">
+              <input type="submit" className="btn btn-info" value="Send Message"></input>
+      </div>
+      </div>
+    </form>
+     </div>
+    </div>
+    </div>
         </Container>
-
-        {/* <div>
+    
+            {/* <div>
                 <Reelcarousel/>
             </div>
              */}
-        {/* <footer  className="footer py-1 bg-white">         
+                {/* <footer  className="footer py-1 bg-white">         
                         <div id="footer" className="container-fluid text-center">© 2020 Copyright:
                             <a href="https://mdbootstrap.com/"> Linden Ustawi</a>
                         </div>
                       
                 </footer>           */}
-      </Container>
-    )
-  }
+        </Container>
+        )
 }
 
-export default Home
+
+export default Home;
